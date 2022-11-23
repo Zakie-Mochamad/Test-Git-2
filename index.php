12353e92862c849c5d442823fdc89d1264945578
Zakie-Mochamad <zakimochamad666@gmail.com>
2022-11-23 23:13:32 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test Git 2</title>
+</head>
 
     <p>IDGAF</p>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi alias rem, voluptatibus eligendi perferendis laudantium aut quisquam debitis maiores accusantium dicta dolorem minima, facilis modi deserunt aspernatur cum ut autem?</p>
@@ -12,9 +13,8 @@
     <ul>
         <li>Hahahaha</li>
         <li>Cuma Test</li>
-        
+
     </ul>
-</head>
 <body>
     
 </body>
