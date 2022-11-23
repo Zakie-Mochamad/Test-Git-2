2abd0fe11ec94e7e68265dfd4541642b8082c38f
Zakie-Mochamad <zakimochamad666@gmail.com>
2022-11-24 00:56:23 +0700

    menambahkan javascript

--- index.php
+++ index.php
@@ -16,6 +16,14 @@
         <li>Cuma Test</li>
 
     </ul>
+
+    <script>
+        
+        for (let index = 0; index < 10; index++) {
+            console.log(index);
+            
+        }
+    </script>
     
 </body>
 </html>
